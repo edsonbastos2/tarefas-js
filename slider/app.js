4b24314a5e2ec0a0eccb34d4a4729d67d2e820f9
Edson Bastos <edsonbastos2@hotmail.com>
2021-11-29 23:59:11 -0300

    Corrigido tempo e retorno da img 0, quando terminaar slider

--- slider/app.js
+++ slider/app.js
@@ -1,16 +1,47 @@
 const previousEl = document.getElementById('previous')
 const nextEl = document.getElementById('next')
 const sliderEl = document.getElementById('slider')
+let interval = undefined
+let timeout = undefined
 
 previousEl.addEventListener('click', onPreviousClick)
 nextEl.addEventListener('click', onNextClick)
 
+autoScroll()
+
 function onPreviousClick(){
     const sliderWidth = sliderEl.offsetWidth
     sliderEl.scrollLeft -= sliderWidth
+    handleSliderClick()
 }
 
 function onNextClick(){
     const sliderWidth = sliderEl.offsetWidth
     sliderEl.scrollLeft += sliderWidth
+    handleSliderClick()
+}
+
+function handleSliderClick(){
+    clearTimeout(timeout)
+    clearInterval(interval)
+    interval = undefined
+
+    timeout = setTimeout(() => {
+        autoScroll()
+    },10000)
+}
+
+function autoScroll(){
+    if(interval) return
+   interval = setInterval(() => {
+        const sliderWidth = sliderEl.offsetWidth
+        const numberImg = sliderEl.childElementCount
+        const selectedImg = (sliderEl.scrollLeft/sliderWidth)+1
+        
+        if(numberImg === selectedImg){
+            sliderEl.scrollLeft = 0
+            return
+        }
+        sliderEl.scrollLeft += sliderWidth
+    }, 5000)
 }
